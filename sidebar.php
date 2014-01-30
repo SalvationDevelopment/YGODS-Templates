1384d237557ca287e3b610d7940d0188fbc9f8ed
James Gladney <jamezs@angelofcode.com>
2014-01-29 23:49:30 -0500

    Changed Templates
    Three days of work after finding a muse.

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<!-- sidebar -->
+<aside class="sidebar" role="complementary">
+
+	<section>
+
+	<div class="sidebar-widget">
+		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
+	</div>
+
+	<div class="sidebar-widget">
+		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
+	</div>
+    </section>
+</aside>
+<!-- /sidebar -->
