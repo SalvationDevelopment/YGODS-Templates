b4691bde7d50b984761a8d3cae926be0e0213cc0
Jamezs Gladney <jamezs@angelofcode.com>
2014-05-11 14:33:06 -0400

    Basic look and feel

--- footer.php
+++ footer.php
@@ -1,30 +1,98 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+<!-- footer -->
+<footer class="footer" role="contentinfo">
 
-				<!-- copyright -->
-				<p class="copyright">
-					&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?>
-					<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
-				</p>
-				<!-- /copyright -->
+    <!-- copyright -->
+    <p class="copyright">
+        &copy;
+        <?php echo date( 'Y'); ?>Copyright
+        <?php bloginfo( 'name'); ?>.
 
-			</footer>
-			<!-- /footer -->
+    </p>
+    <!-- /copyright -->
 
-		</div>
-		<!-- /wrapper -->
+</footer>
+<!-- /footer -->
+<footer id="launcher" style="display:none;">
 
-		<?php wp_footer(); ?>
+    <section id="settings">
+        <div class='section-content'>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+            <div>
+                <h3>Game Settings</h3>
+                <label>Auto Card Placement</label>
+                <input type="checkbox" />
+                <br />
+                <label>Random Card Placement</label>
+                <input type="checkbox" />
+                <br />
+                <label>Auto Chain Order</label>
+                <input type="checkbox" />
+                <br />
+                <label>Enable Sound</label>
+                <input type="checkbox" />
+                <br />
+                <label>Enable DirectX</label>
+                <input type="checkbox" />
+                <br />
+                <label>No Delay for Chains</label>
+                <input type="checkbox" />
+                <br />
+                <label>Enable Music</label>
+                <input type="checkbox" />
+                <br />
+                <label>Fullscreen</label>
+                <input type="checkbox" />
+                <br />
+                <label>Enable User Covers</label>
+                <input type="checkbox" />
+                <br />
+                <label>Antai Alasing</label>
+                <input type="checkbox" />
+                <label>Enable Microphone</label>
+                <input type="checkbox" />
+                <br />
+                <label>Skin</label>
+                <select>
+                    <option value="default">Default</option>
+                </select>
+                <br />
+            </div>
+    </section>
+    <div id='servermessages'>
+        Server Messages will spawn here.
+    </div>
+    <div id="staticcontrols">
+        <div id="LANMode">
+            LAN Mode
+        </div>
+        <div id="rundeckedit" onclick="gotoTab('deckeditor')">
+            Deck Edit
+        </div>
+        <div id="runereplays">
+            Replays
+        </div>
+        <div id="runHost" onclick="gotoTab('language');">
+            Host
+        </div>
 
-	</body>
+    </div>
+</footer>
+
+</div>
+<!-- /wrapper -->
+
+<?php wp_footer(); ?>
+
+<!-- analytics -->
+<script>
+    (function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+    		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+    		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+    		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+    		ga('send', 'pageview');
+</script>
+
+</body>
+
 </html>
--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,57 @@
 /* jslint browser:true */
 /* jslint node:true */
-/* global $, Modernizr */
+/* global $, Modernizr, Primus */
 
 // DOM Ready
-$(function() {
 
-	// SVG fallback
-	// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
-	if (!Modernizr.svg) {
-		var imgs = document.getElementsByTagName('img');
-		for (var i = 0; i < imgs.length; i++) {
-			if (/.*\.svg$/.test(imgs[i].src)) {
-				imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
-			}
-		}
-	}
-    // code starts here.
-    var application = false;
-    if (require){
-        application = true;
-    }else{
-        return false;
+
+// SVG fallback
+// toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script#update
+if (!Modernizr.svg) {
+    var imgs = document.getElementsByTagName('img');
+    for (var i = 0; i < imgs.length; i++) {
+        if (/.*\.svg$/.test(imgs[i].src)) {
+            imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
+        }
     }
-    
-    //display titlebar
-    //bind parts.
+}
+// code starts here.
+var application = false;
+if (require) {
+    application = true;
+} else {
+    return false;
+}
+
+$('#launcher').toggle();
+var primus = Primus.connect('http://salvationdevelopment.com');
+primus.on('data', function message(data) {
+    console.log('Received a new message from the server', data);
 });
+
+//bind parts.
+function closeAllScreens() {
+    $().css('display, none');
+}
+
+function openScreen(id) {
+    closeAllScreens();
+    $(id).toggle();
+}
+
+function joinGamelist() {
+    primus.write({
+        serverEvent: 'joinActiveGames'
+    });
+}
+function leaveGamelist() {
+    primus.write({
+        serverEvent: 'leaveActiveGames'
+    });
+}
+function hostGame(parameters){
+    primus.write({
+        serverEvent: 'hostgame',
+        format: parameters
+    });
+}
